7bfe75375c90d9c7107caca69eaf59e67a55c92d
steph bug <hewsda1@gmail.com>
2018-03-02 12:30:33 +0100

    complete UserProvider contract

--- src/User/UserProvider.php
+++ src/User/UserProvider.php
@@ -9,4 +9,8 @@ use StephBug\SecurityModel\Application\Values\Contract\SecurityIdentifier;
 interface UserProvider
 {
     public function requireByIdentifier(SecurityIdentifier $identifier): UserSecurity;
+
+    public function refreshUser(UserSecurity $user): UserSecurity;
+
+    public function supportsClass(string $class): bool;
 }
