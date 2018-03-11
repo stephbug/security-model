1804ddbdfde14f95f0f042775122ab49d582d9af
steph bug <hewsda1@gmail.com>
2018-03-11 18:30:04 +0100

    simplify return recaller token as string

--- src/User/UserRecaller.php
+++ src/User/UserRecaller.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace StephBug\SecurityModel\User;
 
-use StephBug\SecurityModel\Application\Values\Contract\SecurityIdentifier;
-
 interface UserRecaller
 {
-    public function getRecallerToken(): ?SecurityIdentifier;
+    public function getRecallerToken(): ?string;
 }
