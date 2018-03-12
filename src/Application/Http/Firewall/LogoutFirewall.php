deb709e5131b1e7659d31889c632959dc962ffeb
steph bug <hewsda1@gmail.com>
2018-03-12 18:12:17 +0100

    simplify logout firewall

--- src/Application/Http/Firewall/LogoutFirewall.php
+++ src/Application/Http/Firewall/LogoutFirewall.php
@@ -7,9 +7,9 @@ namespace StephBug\SecurityModel\Application\Http\Firewall;
 use Illuminate\Http\Request;
 use StephBug\SecurityModel\Application\Http\Request\AuthenticationRequest;
 use StephBug\SecurityModel\Application\Http\Response\LogoutSuccess;
-use StephBug\SecurityModel\Application\Values\LogoutHandlers;
 use StephBug\SecurityModel\Guard\Authentication\TrustResolver;
 use StephBug\SecurityModel\Guard\Guard;
+use StephBug\SecurityModel\Guard\Service\Logout\Logout;
 use Symfony\Component\HttpFoundation\Response;
 
 class LogoutFirewall extends AuthenticationFirewall
@@ -20,9 +20,9 @@ class LogoutFirewall extends AuthenticationFirewall
     private $guard;
 
     /**
-     * @var LogoutHandlers
+     * @var array
      */
-    private $logoutHandlers;
+    private $logoutHandlers = [];
 
     /**
      * @var AuthenticationRequest
@@ -40,13 +40,11 @@ class LogoutFirewall extends AuthenticationFirewall
     private $response;
 
     public function __construct(Guard $guard,
-                                LogoutHandlers $logoutHandlers,
                                 AuthenticationRequest $authenticationRequest,
                                 TrustResolver $trustResolver,
                                 LogoutSuccess $response)
     {
         $this->guard = $guard;
-        $this->logoutHandlers = $logoutHandlers;
         $this->authenticationRequest = $authenticationRequest;
         $this->trustResolver = $trustResolver;
         $this->response = $response;
@@ -58,13 +56,22 @@ class LogoutFirewall extends AuthenticationFirewall
 
         $token = $this->guard->requireToken();
 
-        $this->logoutHandlers->processLogout($request, $response, $token);
-
-        $this->guard->event()->dispatchLogoutEvent($token);
+        foreach($this->logoutHandlers as $handler){
+            $handler->processLogout($request, $response, $token);
+        }
 
         $this->guard->forget();
 
+        $this->guard->event()->dispatchLogoutEvent($token);
+
         return $response;
+    }
+
+    public function addHandler(Logout $logoutHandler): LogoutFirewall
+    {
+        $this->logoutHandlers[] = $logoutHandler;
+
+        return $this;
     }
 
     protected function requireAuthentication(Request $request): bool
